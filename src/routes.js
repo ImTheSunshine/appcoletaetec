079c02458e5c427175cde7d46593e69ef5da159e
Debora <deboraribeiror123@gmail.com>
2021-06-07 20:13:23 -0300

    Adicionando a tela de detalhes no projeto

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,26 @@
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 
+
+//importar a paginas
 import Home from './pages/Home';
-import Pontos from './pages/Pontos';
+import Points from './pages/Points';
+import Detail from './pages/Detail';
 
 const Stack = createStackNavigator();
 
-const Routes = () => {
-    return( 
-       <NavigationContainer>
-           <Stack.Navigator>    
+const Routes = () =>{
+    return(
+        <NavigationContainer> 
+            <Stack.Navigator headerMode="none">
                 <Stack.Screen name="Home" component={Home}/>
-                <Stack.Screen name="Pontos" component={Pontos}/>
-           </Stack.Navigator>
-       </NavigationContainer> 
+                <Stack.Screen name="Points" component={Points}/>
+                <Stack.Screen name="Detail" component={Detail}/>
+            </Stack.Navigator>
+        </NavigationContainer>
     )
-}
 
+}
 export default Routes;
